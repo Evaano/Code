0e6475291d781b031d2ff1952ce47bc99492ec33
Evan <evaan0202@gmail.com>
2023-03-11 03:42:15 +0500

    added more sht the lecturer told me to add

---
+++ College/calculator.js
@@ -0,0 +1,18 @@
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+var number1 = 
+var number2 = 
+
+let result;
+
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
