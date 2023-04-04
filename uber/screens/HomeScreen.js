e0286568fa9c64e0522347d69fe23822c27c8baf
Evan <evaan0202@gmail.com>
2023-04-05 01:51:47 +0500

    Uber from wish done

--- uber/screens/HomeScreen.js
+++ uber/screens/HomeScreen.js
@@ -1,12 +1,19 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import tw from 'twrnc'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { Image, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import tw from 'twrnc';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import { setDestination, setOrigin } from '../slices/navSlice';
+import { useDispatch } from 'react-redux';
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
-      <View>
+      <View style={tw`p-5`}>
         <Image
           style={{
             width: 100,
@@ -17,6 +24,38 @@ const HomeScreen = () => {
             uri: 'https://links.papareact.com/gzs',
           }}
         />
+
+        <GooglePlacesAutocomplete
+          placeholder='Current Location?'
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            dispatch(setOrigin({
+              location: details.geometry.location,
+              description: data.description
+            }));
+
+            dispatch(setDestination(null))
+          }}
+          fetchDetails={true}
+          returnKeyType={'search'}
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en'
+          }}
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={400}
+        />
+
+        <NavOptions />
       </View>
     </SafeAreaView>
   );
