ea06ee3551dbf61aecf9302f486ebc0664c8ef06
Evan <evaan0202@gmail.com>
2023-10-02 15:25:15 +0500

    fixed bugs

--- office/inv.py
+++ office/inv.py
@@ -1,14 +1,11 @@
-# Import necessary libraries
 import sqlite3
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 
-# Connect to the SQLite database or create one if it doesn't exist
+# Create or connect to the SQLite database with case-insensitive collation
 conn = sqlite3.connect('inventory.db')
-# Set case-insensitive search for the database
-conn.execute('PRAGMA case_sensitive_like = OFF')
-# Create the 'inventory' table if it doesn't exist
+conn.execute('PRAGMA case_sensitive_like = OFF')  # Ensure case-insensitive behavior
 conn.execute('''
     CREATE TABLE IF NOT EXISTS inventory (
         item_code TEXT PRIMARY KEY,
@@ -27,10 +24,8 @@ conn.execute('''
 conn.commit()
 conn.close()
 
-# Function to add a new item to the inventory
 def add_item():
     def add_item_to_database():
-        # Get data from input fields
         item_code = item_code_entry.get()
         item_description = item_description_entry.get()
         category = category_entry.get()
@@ -43,8 +38,8 @@ def add_item():
         current_stock = current_stock_entry.get()
         reorder = reorder_entry.get().lower() == 'true'
 
+        # Validate and add the item to the database
         try:
-            # Convert numeric fields to integers
             inwards = int(inwards)
             outwards = int(outwards)
             current_stock = int(current_stock)
@@ -52,7 +47,7 @@ def add_item():
             messagebox.showerror("Error", "Invalid input for numeric fields. Item not added.")
             return
 
-        # Connect to the database and insert the item
+        # Insert the new item into the database
         conn = sqlite3.connect('inventory.db')
         cursor = conn.cursor()
         cursor.execute('''
@@ -66,11 +61,11 @@ def add_item():
         messagebox.showinfo("Success", f"Added item {item_description} to the inventory.")
         add_item_dialog.destroy()
 
-    # Create a new dialog window for adding an item
+    # Create a popup dialog for adding an item
     add_item_dialog = tk.Toplevel(root)
     add_item_dialog.title("Add New Item")
 
-    # Create labels and entry fields for item details
+    # Add input fields and labels for item information
     item_code_label = tk.Label(add_item_dialog, text="Item Code:")
     item_code_entry = tk.Entry(add_item_dialog)
     item_description_label = tk.Label(add_item_dialog, text="Item Description:")
@@ -97,7 +92,7 @@ def add_item():
     # Create a button to add the item
     add_button = tk.Button(add_item_dialog, text="Add Item", command=add_item_to_database)
 
-    # Grid layout for labels, entry fields, and the button
+    # Place input fields, labels, and the button in the dialog
     item_code_label.grid(row=0, column=0)
     item_code_entry.grid(row=0, column=1)
     item_description_label.grid(row=1, column=0)
@@ -122,22 +117,23 @@ def add_item():
     reorder_entry.grid(row=10, column=1)
     add_button.grid(row=11, columnspan=2)
 
-# Function to update an existing item in the inventory
 def update_item():
     def update_item_in_database():
         item_code = item_code_entry.get()
 
+        # Connect to the database
         conn = sqlite3.connect('inventory.db')
         cursor = conn.cursor()
 
+        # Check if the item code exists in the database
         cursor.execute("SELECT item_code FROM inventory WHERE item_code=?", (item_code,))
         existing_item = cursor.fetchone()
 
         if existing_item:
+            # Fetch the existing item details
             cursor.execute("SELECT * FROM inventory WHERE item_code=?", (item_code,))
             existing_item_details = cursor.fetchone()
 
-            # Get updated data from input fields, or use existing data if fields are empty
             item_description = item_description_entry.get() or existing_item_details[1]
             category = category_entry.get() or existing_item_details[2]
             subcategory = subcategory_entry.get() or existing_item_details[3]
@@ -146,7 +142,6 @@ def update_item():
             sto_code = sto_code_entry.get() or existing_item_details[6]
 
             try:
-                # Convert numeric fields to integers, or use existing data if fields are empty
                 inwards = int(inwards_entry.get() or existing_item_details[7])
                 outwards = int(outwards_entry.get() or existing_item_details[8])
                 current_stock = int(current_stock_entry.get() or existing_item_details[9])
@@ -171,11 +166,11 @@ def update_item():
             conn.close()
             messagebox.showerror("Error", f"Item with item code {item_code} not found in the inventory.")
 
-    # Create a new dialog window for updating an item
+    # Create a popup dialog for updating an item
     update_item_dialog = tk.Toplevel(root)
     update_item_dialog.title("Update Item")
 
-    # Create labels and entry fields for item details
+    # Add input fields and labels for item information
     item_code_label = tk.Label(update_item_dialog, text="Item Code:")
     item_code_entry = tk.Entry(update_item_dialog)
     item_description_label = tk.Label(update_item_dialog, text="Item Description:")
@@ -202,7 +197,7 @@ def update_item():
     # Create a button to update the item
     update_button = tk.Button(update_item_dialog, text="Update Item", command=update_item_in_database)
 
-    # Grid layout for labels, entry fields, and the button
+    # Place input fields, labels, and the button in the dialog
     item_code_label.grid(row=0, column=0)
     item_code_entry.grid(row=0, column=1)
     item_description_label.grid(row=1, column=0)
@@ -227,19 +222,21 @@ def update_item():
     reorder_entry.grid(row=10, column=1)
     update_button.grid(row=11, columnspan=2)
 
-# Function to delete an item from the inventory
 def delete_item():
     def delete_item_from_database():
         item_code = item_code_entry.get()
 
+        # Connect to the database
         conn = sqlite3.connect('inventory.db')
         cursor = conn.cursor()
 
+        # Check if the item exists in the database
         cursor.execute("SELECT item_description FROM inventory WHERE item_code=?", (item_code,))
         item = cursor.fetchone()
 
         if item:
             item_description = item[0]
+            # Delete the item from the database
             cursor.execute("DELETE FROM inventory WHERE item_code=?", (item_code,))
             conn.commit()
             conn.close()
@@ -249,137 +246,159 @@ def delete_item():
             conn.close()
             messagebox.showerror("Error", f"Item with item code {item_code} not found in the inventory.")
 
-    # Create a new dialog window for deleting an item
+    # Create a popup dialog for deleting an item
     delete_item_dialog = tk.Toplevel(root)
     delete_item_dialog.title("Delete Item")
 
-    # Create a label and an entry field for item code
+    # Add input fields and labels for item code
     item_code_label = tk.Label(delete_item_dialog, text="Item Code:")
     item_code_entry = tk.Entry(delete_item_dialog)
 
     # Create a button to delete the item
     delete_button = tk.Button(delete_item_dialog, text="Delete Item", command=delete_item_from_database)
 
+    # Place input fields, labels, and the button in the dialog
     item_code_label.pack()
     item_code_entry.pack()
     delete_button.pack()
 
-# Function to view the entire inventory
 def view_inventory():
+    # Create a new window for viewing inventory
     view_inventory_window = tk.Toplevel(root)
     view_inventory_window.title("View Inventory")
 
-    # Create a treeview widget to display the inventory data in a table format
-    tree = ttk.Treeview(view_inventory_window, columns=("Item Code", "Item Description", "Category", "Subcategory", "Unit", "Inwards", "Outwards", "Current Stock", "Reorder"), show="headings")
+    # Create a Treeview widget for displaying inventory items
+    tree = ttk.Treeview(view_inventory_window, columns=("Item Code", "Item Description", "Category", "Subcategory", "Unit", "Brand", "STO Code", "Inwards", "Outwards", "Current Stock", "Reorder"), show="headings")
 
-    # Set column headings
-    tree.heading("Item Code", text="Item Code", anchor="w")
+    # Define column headings
+    tree.heading("Item Code", text="Item Code", anchor="center")
     tree.heading("Item Description", text="Item Description", anchor="center")
     tree.heading("Category", text="Category", anchor="center")
     tree.heading("Subcategory", text="Subcategory", anchor="center")
     tree.heading("Unit", text="Unit", anchor="center")
+    tree.heading("Brand", text="Brand", anchor="center")  # Added "Brand" heading
+    tree.heading("STO Code", text="STO Code", anchor="center")
     tree.heading("Inwards", text="Inwards", anchor="center")
     tree.heading("Outwards", text="Outwards", anchor="center")
     tree.heading("Current Stock", text="Current Stock", anchor="center")
     tree.heading("Reorder", text="Reorder", anchor="center")
 
-    # Set column widths
-    tree.column("#1", width=80, anchor="center")
-    tree.column("#2", width=250, anchor="center")
-    tree.column("#3", width=120, anchor="center")
-    tree.column("#4", width=150, anchor="center")
-    tree.column("#5", width=70, anchor="center")
-    tree.column("#6", width=70, anchor="center")
-    tree.column("#7", width=70, anchor="center")
-    tree.column("#8", width=80, anchor="center")
-    tree.column("#9", width=70, anchor="center")
+    # Adjust column widths
+    tree.column("Item Code", width=80, anchor="center")
+    tree.column("Item Description", width=250, anchor="center")
+    tree.column("Category", width=120, anchor="center")
+    tree.column("Subcategory", width=150, anchor="center")
+    tree.column("Unit", width=70, anchor="center")
+    tree.column("Brand", width=100, anchor="center")
+    tree.column("STO Code", width=70, anchor="center")
+    tree.column("Inwards", width=70, anchor="center")
+    tree.column("Outwards", width=70, anchor="center")
+    tree.column("Current Stock", width=80, anchor="center")
+    tree.column("Reorder", width=80, anchor="center")  # Adjust width for the "Reorder" column
 
-    # Connect to the database and fetch all items
+    # Connect to the database
     conn = sqlite3.connect('inventory.db')
     cursor = conn.cursor()
+
+    # Retrieve all items from the database
     cursor.execute("SELECT * FROM inventory")
     items = cursor.fetchall()
+
+    # Close the connection
     conn.close()
 
-    # Insert items into the treeview
     if items:
         for item in items:
-            tree.insert("", "end", values=item)
+            # Format the "Reorder" value as "True" or "False"
+            reorder_value = "True" if item[10] else "False"
+            tree.insert("", "end", values=item[:10] + (reorder_value,))  # Append formatted "Reorder" value
 
-    # Pack the treeview to display it
     tree.pack(fill=tk.BOTH, expand=True)
 
-# Function to search items by name
+
 def search_item_by_name():
     def search_items(event=None):
         search_term = search_name_entry.get().strip().lower()
 
+        # Connect to the database
         conn = sqlite3.connect('inventory.db')
         cursor = conn.cursor()
 
+        # Search for items with descriptions containing the search term (case-insensitive)
         cursor.execute("SELECT * FROM inventory WHERE LOWER(item_description) LIKE ?", ('%' + search_term + '%',))
         items = cursor.fetchall()
 
+        # Close the connection
         conn.close()
 
-        # Clear the existing items in the treeview
+        # Clear previous search results
         for row in tree.get_children():
             tree.delete(row)
 
-        # Insert search results into the treeview
         if items:
             for item in items:
-                tree.insert("", "end", values=item)
+                # Format the "Reorder" value as "True" or "False"
+                reorder_value = "True" if item[10] else "False"
+                tree.insert("", "end", values=item[:10] + (reorder_value,))  # Append formatted "Reorder" value
 
-    # Create a new dialog window for searching items by name
+    # Create a new window for searching items
     search_item_window = tk.Toplevel(root)
     search_item_window.title("Search Items by Name")
 
+    # Create a frame for search input and results
     frame = tk.Frame(search_item_window)
     frame.pack(padx=0, pady=10)
 
-    # Create a label and an entry field for item name search
+    # Create an input field and label for search term
     search_name_label = tk.Label(frame, text="Item Name:")
     search_name_entry = tk.Entry(frame)
 
-    # Bind the Enter key to the search function
+    # Bind the Enter key to initiate the search
     search_name_entry.bind("<Return>", search_items)
 
-    # Create a treeview widget to display search results
-    tree = ttk.Treeview(frame, columns=("Item Code", "Item Description", "Category", "Subcategory", "Unit", "Inwards", "Outwards", "Current Stock", "Reorder"), show="headings")
+    # Create a Treeview widget for displaying search results
+    tree = ttk.Treeview(frame, columns=("Item Code", "Item Description", "Category", "Subcategory", "Unit", "Brand", "STO Code", "Inwards", "Outwards", "Current Stock", "Reorder"), show="headings")
 
-    # Set column headings and widths
+    # Define column headings
     tree.heading("#1", text="Item Code", anchor="center")
     tree.heading("#2", text="Item Description", anchor="center")
     tree.heading("#3", text="Category", anchor="center")
     tree.heading("#4", text="Subcategory", anchor="center")
     tree.heading("#5", text="Unit", anchor="center")
-    tree.heading("#6", text="Inwards", anchor="center")
-    tree.heading("#7", text="Outwards", anchor="center")
-    tree.heading("#8", text="Current Stock", anchor="center")
-    tree.heading("#9", text="Reorder", anchor="center")
+    tree.heading("#6", text="Brand", anchor="center")  # Added "Brand" heading
+    tree.heading("#7", text="STO Code", anchor="center")
+    tree.heading("#8", text="Inwards", anchor="center")
+    tree.heading("#9", text="Outwards", anchor="center")
+    tree.heading("#10", text="Current Stock", anchor="center")
+    tree.heading("#11", text="Reorder", anchor="center")
 
+    # Adjust column widths
     tree.column("#1", width=80, anchor="center")
     tree.column("#2", width=250, anchor="center")
-    tree.column("#3", width=100, anchor="center")
-    tree.column("#4", width=120, anchor="center")
-    tree.column("#5", width=80, anchor="center")
-    tree.column("#6", width=80, anchor="center")
-    tree.column("#7", width=80, anchor="center")
-    tree.column("#8", width=100, anchor="center")
+    tree.column("#3", width=120, anchor="center")
+    tree.column("#4", width=150, anchor="center")
+    tree.column("#5", width=70, anchor="center")
+    tree.column("#6", width=100, anchor="center")
+    tree.column("#7", width=70, anchor="center")
+    tree.column("#8", width=70, anchor="center")
     tree.column("#9", width=80, anchor="center")
+    tree.column("#10", width=80, anchor="center")
+    tree.column("#11", width=80, anchor="center")
 
-    # Create a button to perform the search
+    # Place widgets in the frame
     search_name_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
     search_name_entry.grid(row=0, column=1, padx=5, pady=5)
     tree.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
 
-# Create the main Tkinter window
+
+# Create the main window
 root = tk.Tk()
 root.title("Inventory Management System")
-root.geometry("800x600")
 
-# Create buttons for different operations
+# Set the initial size of the window (width x height)
+root.geometry("800x600")  # Adjust the dimensions as needed
+
+# Create buttons for each operation
 add_button = tk.Button(root, text="Add Item", command=add_item)
 update_button = tk.Button(root, text="Update Item", command=update_item)
 delete_button = tk.Button(root, text="Delete Item", command=delete_item)
@@ -387,7 +406,7 @@ view_button = tk.Button(root, text="View Inventory", command=view_inventory)
 search_button = tk.Button(root, text="Search by Name", command=search_item_by_name)
 exit_button = tk.Button(root, text="Exit", command=root.quit)
 
-# Pack buttons to display them in the main window
+# Place buttons on the window
 add_button.pack()
 update_button.pack()
 delete_button.pack()
@@ -395,5 +414,5 @@ view_button.pack()
 search_button.pack()
 exit_button.pack()
 
-# Start the Tkinter main loop
+# Start the GUI main loop
 root.mainloop()
