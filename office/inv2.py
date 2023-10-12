17a7aeeb104abcc7017e8d27c73646be8fae9c1d
Evan <100699707+Evaano@users.noreply.github.com>
2023-10-12 14:02:17 +0500

    Add files via upload

--- office/inv2.py
+++ office/inv2.py
@@ -3,6 +3,29 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 from PIL import Image, ImageTk
+import pyperclip
+
+# Define the subcategories dictionary at the global level
+subcategories = {
+    "General Items": [
+        "General Items",
+        "Printed Items",
+        "Stationaries",
+        "Toners, Cartridges, and Drums",
+    ],
+    "Medical Items": [
+        "Dental Consumables",
+        "Dialysis",
+        "Laboratory Consumables",
+        "Laboratory Test Kits",
+        "Medical Consumables",
+        "Medical Instruments",
+        "Oncology Items",
+        "Pharmaceuticals",
+        "Radiology",
+    ],
+    "Narcotics": ["Controlled Drugs"],
+}
 
 # Create or connect to the SQLite database with case-insensitive collation
 conn = sqlite3.connect("inventory.db")
@@ -183,10 +206,35 @@ def create_add_item_tab(notebook, tree):
     item_code_entry = tk.Entry(tab)
     item_description_label = tk.Label(tab, text="Item Description:")
     item_description_entry = tk.Entry(tab)
+
     category_label = tk.Label(tab, text="Category:")
-    category_entry = tk.Entry(tab)
+    category_combobox = ttk.Combobox(
+        tab,
+        values=[
+            "General Items",
+            "Medical Items",
+            "Narcotics",
+        ],
+        width="17",
+    )
+    category_combobox.set("General Items")  # Default selection
+
     subcategory_label = tk.Label(tab, text="Subcategory:")
-    subcategory_entry = tk.Entry(tab)
+    subcategory_combobox = ttk.Combobox(
+        tab, values=subcategories["General Items"], width="17"
+    )
+    subcategory_combobox.set("General Items")  # Default selection
+
+    def update_subcategories(event):
+        # Update subcategories Combobox based on the selected category
+        selected_category = category_combobox.get()
+        subcategory_combobox["values"] = subcategories[selected_category]
+        subcategory_combobox.set(
+            subcategories[selected_category][0]
+        )  # Set the first subcategory as default
+
+    category_combobox.bind("<<ComboboxSelected>>", update_subcategories)
+
     unit_label = tk.Label(tab, text="Unit:")
     unit_entry = tk.Entry(tab)
     brand_label = tk.Label(tab, text="Brand (Optional):")
@@ -222,8 +270,8 @@ def create_add_item_tab(notebook, tree):
         command=lambda: add_item_to_database(
             item_code_entry.get(),
             item_description_entry.get(),
-            category_entry.get(),
-            subcategory_entry.get(),
+            category_combobox.get(),
+            subcategory_combobox.get(),
             unit_entry.get(),
             brand_entry.get(),
             inwards_entry.get(),
@@ -243,25 +291,25 @@ def create_add_item_tab(notebook, tree):
     )
     item_description_entry.grid(row=1, column=1, pady=(0, 0))
     category_label.grid(row=2, column=0, pady=(0, 0), padx=(padx_left, 0), sticky="w")
-    category_entry.grid(row=2, column=1, pady=(0, 0))
+    category_combobox.grid(row=2, column=1, pady=(0, 0))
     subcategory_label.grid(
         row=3, column=0, pady=(0, 0), padx=(padx_left, 0), sticky="w"
     )
-    subcategory_entry.grid(row=3, column=1, pady=(0, 0))
+    subcategory_combobox.grid(row=3, column=1, pady=(0, 0))
     unit_label.grid(row=4, column=0, pady=(0, 0), padx=(padx_left, 0), sticky="w")
     unit_entry.grid(row=4, column=1, pady=(0, 0))
     brand_label.grid(row=5, column=0, pady=(0, 0), padx=(padx_left, 0), sticky="w")
     brand_entry.grid(row=5, column=1, pady=(0, 0))
-    inwards_label.grid(row=7, column=0, pady=(0, 0), padx=(padx_left, 0), sticky="w")
-    inwards_entry.grid(row=7, column=1, pady=(0, 0))
-    outwards_label.grid(row=8, column=0, pady=(0, 0), padx=(padx_left, 0), sticky="w")
-    outwards_entry.grid(row=8, column=1, pady=(0, 0))
+    inwards_label.grid(row=6, column=0, pady=(0, 0), padx=(padx_left, 0), sticky="w")
+    inwards_entry.grid(row=6, column=1, pady=(0, 0))
+    outwards_label.grid(row=7, column=0, pady=(0, 0), padx=(padx_left, 0), sticky="w")
+    outwards_entry.grid(row=7, column=1, pady=(0, 0))
     current_stock_label.grid(
-        row=9, column=0, pady=(0, 0), padx=(padx_left, 0), sticky="w"
+        row=8, column=0, pady=(0, 0), padx=(padx_left, 0), sticky="w"
     )
-    current_stock_entry.grid(row=9, column=1, pady=(0, 0))
-    reorder_label.grid(row=10, column=0, pady=(0, 0), padx=(padx_left, 0), sticky="w")
-    reorder_entry.grid(row=10, column=1, pady=(0, 0))
+    current_stock_entry.grid(row=8, column=1, pady=(0, 0))
+    reorder_label.grid(row=9, column=0, pady=(0, 0), padx=(padx_left, 0), sticky="w")
+    reorder_entry.grid(row=9, column=1, pady=(0, 0))
     add_button.grid(row=11, columnspan=2, pady=(pady_top, 0), padx=(padx_left, 0))
 
 
@@ -334,6 +382,8 @@ def create_view_inventory_tab(notebook):
             )  # Append formatted "Reorder" value
 
     tree.pack(fill=tk.BOTH, expand=True)
+    tree.pack(fill=tk.BOTH, expand=True)
+    tree.bind('<Control-c>', lambda event: copy_selected_cell(event, tree))
 
     return tree  # Return the tree widget
 
@@ -389,6 +439,276 @@ def create_delete_item_tab(notebook, tree):
     delete_button.grid(row=1, columnspan=2, pady=(20, 0), padx=(55, 0))
 
 
+def update_item_in_database(
+    item_code,
+    item_description,
+    category,
+    subcategory,
+    unit,
+    brand,
+    inwards,
+    outwards,
+    current_stock,
+    reorder,
+    tree,
+):
+    # Validate and update the item in the database
+    try:
+        inwards = int(inwards)
+        outwards = int(outwards)
+        current_stock = int(current_stock)
+    except ValueError:
+        messagebox.showerror(
+            "Error", "Invalid input for numeric fields. Item not updated."
+        )
+        return
+
+    # Connect to the database
+    conn = sqlite3.connect("inventory.db")
+    cursor = conn.cursor()
+
+    # Update the item in the database
+    cursor.execute(
+        """
+        UPDATE inventory
+        SET item_description=?, category=?, subcategory=?, unit=?, brand=?, inwards=?, outwards=?, current_stock=?, reorder=?
+        WHERE item_code=?
+        """,
+        (
+            item_description,
+            category,
+            subcategory,
+            unit,
+            brand,
+            inwards,
+            outwards,
+            current_stock,
+            reorder,
+            item_code,
+        ),
+    )
+    conn.commit()
+    conn.close()
+
+    messagebox.showinfo("Success", f"Updated item with Item Code {item_code}.")
+
+    # Refresh the inventory Treeview after updating the item
+    refresh_inventory_tree(tree)
+
+
+def search_item_and_fill_fields(
+    search_entry,
+    tree,
+    item_code_entry,
+    item_description_entry,
+    category_combobox,
+    subcategory_combobox,
+    unit_entry,
+    brand_entry,
+    inwards_entry,
+    outwards_entry,
+    current_stock_entry,
+    reorder_entry,
+):
+    item_to_update = search_entry.get()
+
+    # Connect to the database
+    conn = sqlite3.connect("inventory.db")
+    cursor = conn.cursor()
+
+    # Check if the item with the given item code or item name exists
+    cursor.execute(
+        "SELECT * FROM inventory WHERE item_code=? OR item_description=?",
+        (item_to_update, item_to_update),
+    )
+    existing_item = cursor.fetchone()
+
+    if existing_item:
+        (
+            item_code,
+            item_description,
+            category,
+            subcategory,
+            unit,
+            brand,
+            inwards,
+            outwards,
+            current_stock,
+            reorder,
+        ) = existing_item
+        item_code_entry.delete(0, tk.END)
+        item_code_entry.insert(0, item_code)
+        item_description_entry.delete(0, tk.END)
+        item_description_entry.insert(0, item_description)
+        category_combobox.delete(0, tk.END)
+        category_combobox.insert(0, category)
+        subcategory_combobox.delete(0, tk.END)
+        subcategory_combobox.insert(0, subcategory)
+        unit_entry.delete(0, tk.END)
+        unit_entry.insert(0, unit)
+        brand_entry.delete(0, tk.END)
+        brand_entry.insert(0, brand)
+        inwards_entry.delete(0, tk.END)
+        inwards_entry.insert(0, inwards)
+        outwards_entry.delete(0, tk.END)
+        outwards_entry.insert(0, outwards)
+        current_stock_entry.delete(0, tk.END)
+        current_stock_entry.insert(0, current_stock)
+        reorder_entry.delete(0, tk.END)
+        reorder_entry.insert(0, "True" if reorder else "False")
+    else:
+        conn.close()
+        messagebox.showerror("Error", "Item not found.")
+        return
+
+    conn.close()
+
+
+def create_update_item_tab(notebook, tree):
+    # Create a new tab for updating an item
+    tab = ttk.Frame(notebook)
+    notebook.add(tab, text="Update Item")
+
+    # Add input fields and labels for search and item information
+    search_label = tk.Label(tab, text="Search by Item Code or Item Name:")
+    search_entry = tk.Entry(tab)
+    search_button = tk.Button(
+        tab,
+        text="Search",
+        command=lambda: search_item_and_fill_fields(
+            search_entry,
+            tree,
+            item_code_entry,
+            item_description_entry,
+            category_combobox,
+            subcategory_combobox,
+            unit_entry,
+            brand_entry,
+            inwards_entry,
+            outwards_entry,
+            current_stock_entry,
+            reorder_entry,
+        ),
+    )
+
+    # Add labels for item fields
+    item_code_label = tk.Label(tab, text="Item Code:")
+    item_code_entry = tk.Entry(tab)
+    item_description_label = tk.Label(tab, text="Item Description:")
+    item_description_entry = tk.Entry(tab)
+    category_label = tk.Label(tab, text="Category:")
+    category_combobox = ttk.Combobox(
+        tab, values=["General Items", "Medical Items", "Narcotics"], width="17"
+    )
+    subcategory_label = tk.Label(tab, text="Subcategory:")
+    subcategory_combobox = ttk.Combobox(
+        tab, values=subcategories["General Items"], width="17"
+    )
+
+    def update_subcategories(event):
+        # Update subcategories Combobox based on the selected category
+        selected_category = category_combobox.get()
+        subcategory_combobox["values"] = subcategories[selected_category]
+        subcategory_combobox.set(
+            subcategories[selected_category][0]
+        )  # Set the first subcategory as default
+
+    category_combobox.bind("<<ComboboxSelected>>", update_subcategories)
+    unit_label = tk.Label(tab, text="Unit:")
+    unit_entry = tk.Entry(tab)
+    brand_label = tk.Label(tab, text="Brand (Optional):")
+    brand_entry = tk.Entry(tab)
+    inwards_label = tk.Label(tab, text="Inwards:")
+    inwards_entry = tk.Entry(tab)
+    outwards_label = tk.Label(tab, text="Outwards:")
+    outwards_entry = tk.Entry(tab)
+    current_stock_label = tk.Label(tab, text="Current Stock:")
+    current_stock_entry = tk.Entry(tab)
+    reorder_label = tk.Label(tab, text="Reorder (True/False):")
+    reorder_entry = tk.Entry(tab)
+
+    # Set text alignment to left for labels
+    for label in [
+        search_label,
+        item_code_label,
+        item_description_label,
+        category_label,
+        subcategory_label,
+        unit_label,
+        brand_label,
+        inwards_label,
+        outwards_label,
+        current_stock_label,
+        reorder_label,
+    ]:
+        label.configure(justify="left")
+
+    # Create a button to update the item
+    update_button = tk.Button(
+        tab,
+        text="Update Item",
+        command=lambda: update_item_in_database(
+            item_code_entry.get(),
+            item_description_entry.get(),
+            category_combobox.get(),
+            subcategory_combobox.get(),
+            unit_entry.get(),
+            brand_entry.get(),
+            inwards_entry.get(),
+            outwards_entry.get(),
+            current_stock_entry.get(),
+            reorder_entry.get().lower() == "true",
+            tree,
+        ),
+    )
+
+    search_label.grid(row=0, column=0, pady=(20, 0), padx=(55, 0), sticky="w")
+    search_entry.grid(row=0, column=1, pady=(20, 0))
+    search_button.grid(row=1, columnspan=2, pady=(10, 0), padx=(55, 0))
+    item_code_label.grid(row=2, column=0, pady=(20, 0), padx=(55, 0), sticky="w")
+    item_code_entry.grid(row=2, column=1, pady=(20, 0))
+    item_description_label.grid(row=3, column=0, pady=(0, 0), padx=(55, 0), sticky="w")
+    item_description_entry.grid(row=3, column=1, pady=(0, 0))
+    category_label.grid(row=4, column=0, pady=(0, 0), padx=(55, 0), sticky="w")
+    category_combobox.grid(row=4, column=1, pady=(0, 0))
+    subcategory_label.grid(row=5, column=0, pady=(0, 0), padx=(55, 0), sticky="w")
+    subcategory_combobox.grid(row=5, column=1, pady=(0, 0))
+    unit_label.grid(row=6, column=0, pady=(0, 0), padx=(55, 0), sticky="w")
+    unit_entry.grid(row=6, column=1, pady=(0, 0))
+    brand_label.grid(row=7, column=0, pady=(0, 0), padx=(55, 0), sticky="w")
+    brand_entry.grid(row=7, column=1, pady=(0, 0))
+    inwards_label.grid(row=8, column=0, pady=(0, 0), padx=(55, 0), sticky="w")
+    inwards_entry.grid(row=8, column=1, pady=(0, 0))
+    outwards_label.grid(row=9, column=0, pady=(0, 0), padx=(55, 0), sticky="w")
+    outwards_entry.grid(row=9, column=1, pady=(0, 0))
+    current_stock_label.grid(row=10, column=0, pady=(0, 0), padx=(55, 0), sticky="w")
+    current_stock_entry.grid(row=10, column=1, pady=(0, 0))
+    reorder_label.grid(row=11, column=0, pady=(0, 0), padx=(55, 0), sticky="w")
+    reorder_entry.grid(row=11, column=1, pady=(0, 0))
+    update_button.grid(row=12, columnspan=2, pady=(20, 0), padx=(55, 0))
+
+    return (
+        item_code_entry,
+        item_description_entry,
+        category_combobox,
+        subcategory_combobox,
+        unit_entry,
+        brand_entry,
+        inwards_entry,
+        outwards_entry,
+        current_stock_entry,
+        reorder_entry,
+    )
+
+
+def copy_selected_cell(event, tree):
+    selected_item = tree.item(tree.selection())
+    if selected_item:
+        selected_cell = selected_item["values"]
+        selected_cell_text = "\t".join(str(cell) for cell in selected_cell)
+        pyperclip.copy(selected_cell_text)
+
+
 def main():
     # Create the main window
     root = tk.Tk()
@@ -401,9 +721,10 @@ def main():
     start_frame = create_start_page(root, notebook)
 
     # Create tabs for different functionality
-    tree = create_view_inventory_tab(notebook)  # Assign the created tree widget
+    tree = create_view_inventory_tab(notebook)
     create_add_item_tab(notebook, tree)
-    create_delete_item_tab(notebook, tree)  # Pass the "tree" to the delete tab
+    create_delete_item_tab(notebook, tree)
+    create_update_item_tab(notebook, tree)
 
     # Start the GUI main loop
     root.mainloop()
